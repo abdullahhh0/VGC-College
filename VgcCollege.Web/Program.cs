826c936ec14401af87af9dd603bc7d2363284d7b
Abdullah Bin Nasir <165468513+abdullahhh0@users.noreply.github.com>
2026-05-28 14:52:28 +0500

    Database migration and seeding

--- VgcCollege.Web/Program.cs
+++ VgcCollege.Web/Program.cs
@@ -14,21 +14,32 @@ if (!File.Exists(targetDb) && File.Exists(sourceDb))
 }
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// AUTO CREATE DATABASE + TABLES
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    db.Database.Migrate();
+
+    await SeedData.InitialiseAsync(scope.ServiceProvider);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -56,10 +67,5 @@ app.MapControllerRoute(
 
 app.MapRazorPages()
     .WithStaticAssets();
-
-using (var scope = app.Services.CreateScope())
-{
-    await SeedData.InitialiseAsync(scope.ServiceProvider);
-}
 
 app.Run();
